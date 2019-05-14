3f474be1336c0cb4f99016860d96c383ea3027f7
chenxm <herewinner@163.com>
2019-05-14 22:29:59 +0800

    circle

--- swipedissmiss/src/main/java/com/terry/swipedissmiss/CloudTerraceView.java
+++ swipedissmiss/src/main/java/com/terry/swipedissmiss/CloudTerraceView.java
@@ -1,7 +1,9 @@
 package com.terry.swipedissmiss;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
 import androidx.annotation.Nullable;
@@ -13,6 +15,11 @@ import androidx.annotation.Nullable;
  * Description:
  */
 public class CloudTerraceView extends View {
+
+    private int mRadius;
+
+    private static final String TAG_CXM = "cxm";
+
     public CloudTerraceView(Context context) {
         this(context, null);
     }
@@ -23,6 +30,8 @@ public class CloudTerraceView extends View {
 
     public CloudTerraceView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        TypedArray attributes = context.getResources().obtainAttributes(attrs, R.styleable.CloudTerraceView);
+        attributes.getColor(R.styleable.CloudTerraceView_circle_bg, context.getResources().getColor(R.color.ciclr_bg));
         initData();
     }
 
@@ -36,7 +45,22 @@ public class CloudTerraceView extends View {
         int widthMode = MeasureSpec.getMode(widthMeasureSpec);
         int heightSize = MeasureSpec.getSize(heightMeasureSpec);
         int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+        Log.i(TAG_CXM, "widthMode = " + widthMode + ", width = " + widthSize + ", heigtMode = " + heightMode + " height = " + heightSize + " --- EXACTLY = " + MeasureSpec.EXACTLY + " ---AT_MOST = " + MeasureSpec.AT_MOST + "----UNSPECIFIED = " + MeasureSpec.UNSPECIFIED);
+        int width = 0,height = 0;
+        if (widthSize > heightSize) {
+            width = height = heightSize;
+        }
+        if (heightSize > widthSize) {
+            width = height = widthSize;
+        }
+        setMeasuredDimension(width, height);
+    }
 
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        Log.w(TAG_CXM, "w = " + w + ", h = " + h + ", oldW = " + oldw + ", oldH = " + oldh);
+        mRadius = Math.min(w, h) / 2;
     }
 }
